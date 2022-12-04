d176dafe6c98a8f2bc45601f3b2063e4000b855d
ahnjs427 <jinseok.ahn@outlook.com>
2022-12-04 15:33:33 +0900

    update

--- gist/disk_usage.py
+++ gist/disk_usage.py
@@ -4,14 +4,16 @@ import os
 
 show_name = 'pipe'
 
-publish_2d_list = ['2d/assets', '2d/global', '2d/shots']
-publish_3d_list = ['3d/assets', '3d/global', '3d/shots']
-screening_list  = ['*']
-stuff_list      = ['*']
-works_list      = ['*']
+my_dict = { 
+    'publish'   : ['2d/assets', '2d/global', '2d/shots',
+                   '3d/assets', '3d/global', '3d/shots',],
+    'screening' : [''],
+    'stuff'     : [''],
+    'works'     : [''],
+}
 
 
-def convert_size(size_bytes):
+def convert_size(size_bytes): # https://stackoverflow.com/questions/5194057/better-way-to-convert-file-sizes-in-python
     import math
     if size_bytes == 0:
         return '0B'
@@ -20,6 +22,7 @@ def convert_size(size_bytes):
     p = math.pow(1024, i)
     s = round(size_bytes / p, 2)
     return f'{s} {size_name[i]}'
+
 
 def get_dir_size(path='.'):
     total = 0
@@ -32,11 +35,29 @@ def get_dir_size(path='.'):
     return total
 
 
-for directory in publish_3d_list:
-    pull_path = f'/show/{show_name}/publish/{directory}'
-    try:
-        result = get_dir_size(pull_path)
-        print(f'{show_name}/publish/{directory}', ':', convert_size(result))
-    except:
-        print(pull_path[6:], ':', '0 GB')
+for index, key in enumerate(my_dict.keys(), start=1):
+    front_path = f'/show/{show_name}/{key}'
+    if index == 1:
+        for back_path in my_dict[key]:
+            full_path = front_path + '/' + back_path
+            try:
+                raw_result = get_dir_size(full_path)
+                print(full_path, ':', convert_size(raw_result))
+            except:
+                print(full_path,  ':', '\033[31m' + '0 GB' + '\033[0m')
+    else:
+        full_path = front_path
+        try:
+            raw_result = get_dir_size(full_path)
+            print(full_path, ':', convert_size(raw_result))
+        except:
+            print(full_path,  ':', '\033[31m' + '0 GB' + '\033[0m')
+
+        
+        
+
+        
+        
+    
+
     
--- scripts/crontab_start_instance.py
+++ scripts/crontab_start_instance.py
@@ -1,17 +1,12 @@
-#! /usr/bin/python3
+#! /usr/local/bin/python3.9
 
 import time
 import boto3
 
 region = 'ap-northeast-2'
 ec2 = boto3.client('ec2', region_name=region)
-instances = [
-             'i-0adc529bbe14bd1d7',         #renderfarm-01
-            #  'i-0bd160a64359c65b1',         #renderfarm-02
-            #  'i-0222307a6c89a2665',         #renderfarm-03
-            #  'i-0fb59d0ee3b63ace7',         #renderfarm-04
-             'i-0aedf0d4ccad67be9',         #renderfarm-05
 
+instances_vdi = [
              'i-0b2fbdf3602807d28',         #VDI-PC-01
              'i-0f7375d22aa29ed32',         #VDI-PC-02
              'i-09a97b01606de4b71',         #VDI-PC-03
@@ -21,14 +16,40 @@ instances = [
              'i-0922dc46e95339f2b',         #VDI-PC-07
              'i-069b14073a2e65356',         #VDI-PC-08
              'i-0918baf2bacf3740c',         #VDI-PC-09
+]
 
-            #  'i-06fa2924c38b72379',     #VDI-PC-10 : TD-a
-            #  'i-04adda334f488ea0b',     #VDI-PC-11 : TD-b
-            #  'i-04532fbb62e9a474d',     #VDI-PC-12 : TD-c
-            #  'i-0ff37626bf86d68df',     #VDI-PC-13 : for NUKE & HIERO
-            #  'i-06da6ceca7113d60d',     #VDI-PC-14 : Omniverse-windows
+instances_farm = [ 
+             'i-0adc529bbe14bd1d7',         #renderfarm-01
+             'i-0bd160a64359c65b1',         #renderfarm-02
+             'i-0222307a6c89a2665',         #renderfarm-03
+             'i-0fb59d0ee3b63ace7',         #renderfarm-04
+             'i-0aedf0d4ccad67be9',         #renderfarm-05
+             'i-02fb5214cc7987e7c',
+             'i-055cb465edf16a9f3',
+             'i-0a00b4e240c5d8a24',
+             'i-0291ec33eeac9052b',
+             'i-06be0e7d5e462dc96',
+             'i-0e5dae3bd6798946a',
+             'i-06e98657b134ddc02',
+             'i-0cd5a30b9582e8b88',
+             'i-0e7a7ba97c213da3b',
+             'i-07349bc418f4529b8',
+]
+# start VDI - g4dn Family
+ec2.start_instances(InstanceIds=instances_vdi)
 
-            #  'i-0a5d83de33c0462cf',     #Omniverse-Nucleus
-            ]
+# start FARM - c4 Family
 
-ec2.start_instances(InstanceIds=instances)
+while True:
+    time.sleep(60)
+    try:
+        print('try...')
+        ec2.start_instances(InstanceIds=instances_farm)
+    except Exception as e:
+        print(e)
+    else:
+        print('Success!')
+    finally:
+        ec2.start_instances(InstanceIds=instances_farm)
+        print('Retry...')
+    
